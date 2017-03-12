c2873d06ed4faeb13e28692b0b2c1b26036da394
Christopher LaPointe <cmlapointe11@gmail.com>
2017-03-12 18:05:49 -0400

    Add utilities for deep walking structures

--- util/deep.js
+++ util/deep.js
@@ -1,7 +1,34 @@
 const _ = require('lodash');
 
-export default {
-	deepFilter(object, matcher) {
-		
+function __collapse(obj, path, accum) {
+	if (_.isPlainObject(obj)) {
+		_.forEach(obj, (val, key) => {
+			__collapse(val, `${path}${path ? '.' : ''}${key}`, accum);
+		});
+	} else if (_.isArray(obj)) {
+		_.forEach(obj, (val, index) => {
+			__collapse(val, `${path}[${index}]`, accum);
+		})
+	} else {
+		accum[path] = obj;
 	}
+}
+
+function collapse(obj) {
+	let accum = {};
+	__collapse(obj, 'root', accum);
+	return accum;
+}
+
+function expand(obj) {
+	let expanded = {};
+	_.forEach(obj, (val, key) => {
+		_.set(expanded, key, val);
+	})
+	return expanded.root;
+}
+
+module.exports = {
+	collapse,
+	expand
 };
