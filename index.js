7b34b0e55e58ed8a9d40c20ec1e6098ce4cd5e79
Christopher LaPointe <cmlapointe11@gmail.com>
2017-03-26 22:56:27 -0400

    Refactor some code out of the main index

--- index.js
+++ index.js
@@ -1,49 +1,10 @@
-const _ = require('lodash');
-const promise = require('bluebird');
+const Promise = require('bluebird');
 const runner = require('./runner');
-const log = require('winston');
-const validator = require('./expectations').validate;
-const getAllCombinations = require('./util/combiner').getAllCombinations;
-
-const DEFAULT_TIMEOUT = 4000;
 
 const MOCHA_MODE = typeof describe !== 'undefined';
 let __currentConfig = {};
 const __tests = [];
 
-
-function buildTestVariations(testSet) {
-  return _.reduce(testSet, (accum, test) => _.union(accum, getAllCombinations(test)), []);
-}
-
-/* Haven't made this work in a while
-function buildMochaTests(apiSpec) {
-  const description = apiSpec.description ||
-  _.join(_.map(apiSpec.steps, step => `${step.method} ${step.uri}`), ' -> ');
-  const tagStr = _.join(_.map(apiSpec.tags || [], tag => `@${tag}`), ', ');
-
-  describe(`${tagStr}: ${description}`, () => {
-    _.forEach(buildTestVariations(apiSpec.tests), (test) => {
-      it(`Test with values: ${JSON.stringify(test)}`, done => {
-        this.timeout(apiSpec.timeout || DEFAULT_TIMEOUT);
-        runner.executeAllStepsAync(test, apiSpec.steps, apiSpec.config, validator)
-          .nodeify(done);
-      });
-    });
-  });
-}
-*/
-
-function runTestsInteractively(apiSpec) {
-  log.info(`Running test: ${apiSpec.spec.description || 'Undefined description'}`);
-  const variations = buildTestVariations(apiSpec.spec.tests);
-  console.dir(variations);
-  return promise.map(variations, test =>
-    runner.executeAllStepsAync(test, apiSpec.spec.steps, _.merge({}, apiSpec.config, test), validator)
-      .catch((err) => {
-        log.warn(`Error processing ${apiSpec.spec.description}: ${err}`);
-      }), { concurrency: 1 });
-}
 
 /**
 * This is the entry-point for all tests
@@ -51,12 +12,12 @@ function runTestsInteractively(apiSpec) {
 function describeConsistently(apiSpec) {
   __tests.push({ spec: apiSpec, config: __currentConfig });
   if (MOCHA_MODE) {
-    buildMochaTests(apiSpec);
+    //buildMochaTests(apiSpec);
   }
 }
 
 function evaluateApis() {
-  return promise.map(__tests, runTestsInteractively, { concurrency: 1 });
+  return Promise.map(__tests, runner.runTestsInteractively, { concurrency: 1 });
 }
 const wrapper = (opts, func) => {
   __currentConfig = opts;
--- runner/index.js
+++ runner/index.js
@@ -1,1 +1,45 @@
-module.exports = require('./apiSpecRunner');
+const _ = require('lodash');
+const Promise = require('bluebird');
+const log = require('winston');
+const runner = require('./apiSpecRunner');
+const getAllCombinations = require('../util/combiner').getAllCombinations;
+const validator = require('../expectations').validate;
+
+function buildTestVariations(testSet) {
+  return _.reduce(testSet, (accum, test) => _.union(accum, getAllCombinations(test)), []);
+}
+
+/* Haven't made this work in a while
+function buildMochaTests(apiSpec) {
+  const DEFAULT_TIMEOUT = 4000;
+  const description = apiSpec.description ||
+  _.join(_.map(apiSpec.steps, step => `${step.method} ${step.uri}`), ' -> ');
+  const tagStr = _.join(_.map(apiSpec.tags || [], tag => `@${tag}`), ', ');
+
+  describe(`${tagStr}: ${description}`, () => {
+    _.forEach(buildTestVariations(apiSpec.tests), (test) => {
+      it(`Test with values: ${JSON.stringify(test)}`, done => {
+        this.timeout(apiSpec.timeout || DEFAULT_TIMEOUT);
+        runner.executeAllStepsAync(test, apiSpec.steps, apiSpec.config, validator)
+          .nodeify(done);
+      });
+    });
+  });
+}
+*/
+
+function runTestsInteractively(apiSpec) {
+  log.info(`Running test: ${apiSpec.spec.description || 'Undefined description'}`);
+  const variations = buildTestVariations(apiSpec.spec.tests);
+  console.dir(variations);
+  return Promise.map(variations, test =>
+    runner.executeAllStepsAync(test, apiSpec.spec.steps, _.merge({}, apiSpec.config, test), validator)
+      .catch((err) => {
+        log.warn(`Error processing ${apiSpec.spec.description}: ${err}`);
+      }), { concurrency: 1 });
+}
+
+module.exports = {
+  apiSpecRunner: runner,
+  runTestsInteractively
+};
