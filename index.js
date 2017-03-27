78de5d0c9754a6ccbba457c5eaaf2b8b46a90e30
Christopher LaPointe <cmlapointe11@gmail.com>
2017-03-26 23:02:45 -0400

    Fix lint issues

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const MOCHA_MODE = typeof describe !== 'undefined';
 let __currentConfig = {};
 const __tests = [];
 
+const wrapper = (opts, func) => {
+  __currentConfig = opts;
+  func();
+  __currentConfig = {};
+};
 
 /**
 * This is the entry-point for all tests
@@ -12,18 +17,14 @@ const __tests = [];
 function describeConsistently(apiSpec) {
   __tests.push({ spec: apiSpec, config: __currentConfig });
   if (MOCHA_MODE) {
-    //buildMochaTests(apiSpec);
+    // buildMochaTests(apiSpec);
   }
 }
 
 function evaluateApis() {
   return Promise.map(__tests, test => runner.runTestsInteractively(test, wrapper.globalConfig), { concurrency: 1 });
 }
-const wrapper = (opts, func) => {
-  __currentConfig = opts;
-  func();
-  __currentConfig = {};
-};
+
 wrapper.describeConsistently = describeConsistently;
 wrapper.evaluateApis = evaluateApis;
 wrapper.globalConfig = {};
